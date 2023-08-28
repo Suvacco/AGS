b2c9bfc5b5bb54a72f3de6e1793983aa9a3c7328
Gustavo <104271289+GustavoAndradeGit@users.noreply.github.com>
2023-08-27 22:35:00 -0300

    Começo do menu

--- implementacao/AGS/src/Main.java
+++ implementacao/AGS/src/Main.java
@@ -31,5 +31,75 @@ public class Main {
         Pessoa pes1 = new Pessoa("14", "123", "Joao Paulo", new Aluno());
         Disciplina dis1 = new Disciplina("HISTORIA", "14");
         ((Aluno) pes1.getTipo()).matricularEmDisciplina(dis1);
+
+//        int option = 0;
+//        Scanner read = new Scanner(System.in);
+//        UsuarioManager usuarioManager = new UsuarioManager();
+//        
+//        while(option != 99) {
+//        	
+//        	printMenu();
+//        	
+//        	System.out.println("Opção: ");
+//        	option = read.nextInt();
+//        	
+//        	switch(option) {
+//        	
+//        		case 1:
+//        			
+//        			try {
+//						usuarioManager.carregarUsuarios("database/usuarios.csv");
+//        	        } catch (FileNotFoundException e) {
+//        	            System.out.print("Arquivo não encontrado: " + e.getMessage());
+//        	        }
+//        			
+//        			break;
+//        			
+//        		case 2: 
+//        			
+//        			String pessoa[] = new String[4];
+//        			
+//        			
+//        			break;
+//        			
+//        		case 3: 
+//        			
+//        			break;
+//        			
+//        		case 4: 
+//        			
+//        			break;
+//        			
+//        		case 5:
+//        			
+//        			break;
+//        			
+//        		case 6:
+//        			
+//        			break;
+//        			
+//        		case 7:
+//        			
+//        			break;
+//        	}
+//        }
     }
+
+//      public static void printMenu() {
+//    	System.out.println("\n--------------------------------------------------\n");
+//        System.out.println("---------------------- MENU ----------------------\n");
+//        System.out.println("Escolha a opção que deseja:");
+//        System.out.println("Digite 99 para sair:\n");
+//        System.out.println("------------------- Usuarios -------------------");
+//        System.out.println("1. Carregar usuarios");
+//        System.out.println("2. Adicionar usuario ao sistema\n");
+//        System.out.println("------------------- Matriculas -------------------");
+//        System.out.println("3. Cadastrar matricula de aluno");
+//        System.out.println("4. Carregar matriculas\n");
+//        System.out.println("------------------- Disciplinas -------------------");
+//        System.out.println("5. Adicionar disciplina");
+//        System.out.println("6. Remover disciplina");
+//        System.out.println("7. Renomear disciplina\n");
+//    }
+
 }
